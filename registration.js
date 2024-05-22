cbbf53b394545f7dbd2bde94ef47deb07cedd755
Howaitoniksu <129360255+Howaitoniksu@users.noreply.github.com>
2024-05-22 21:56:44 +0300

    Connection with API, make Register

--- registration.js
+++ registration.js
@@ -1,3 +1,25 @@
-async function register (){
-    
+async function register() {
+    const response = await fetch('http://localhost:8000/api/auth/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            name: document.getElementById('name').value,
+            surname: document.getElementById('surname').value,
+            phone_number: document.getElementById('phone').value,
+            email: document.getElementById('email').value,
+            password: document.getElementById('password').value
+        })
+    })
+
+    const data = await response.json()
+
+    if (!data.success) {
+        return;
+    }
+
+    localStorage.setItem('token', data.token)
+
+    window.location.href = 'main.html'
 }
