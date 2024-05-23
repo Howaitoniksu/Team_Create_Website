3c8a890b5812fb8cd5c57b9b6ee6baf0523e19f7
Howaitoniksu <129360255+Howaitoniksu@users.noreply.github.com>
2024-05-23 08:44:34 +0300

    "Edit some API conn"

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use GuzzleHttp\Client;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function getUserByToken(Request $request)
+    public function getUserByToken(Request $request) : JsonResponse
     {
         if (!$request->hasHeader('Authorization')) {
             return response()->json(['error' => 'Unauthorized'], 401);
@@ -23,19 +23,19 @@ class UserController extends Controller
         return response()->json($user);
     }
 
-    public function sendCode(Request $request)
-    {
-        $client = new Client();
-        $response = $client->request('POST', 'https://iis.ngknn.ru/NGKNN/МамшеваЮС/MedicMadlab/api/SendCode', [
-            'headers' => [
-                'User-email' => $request->all()['email']
-            ]
-        ]);
-
-        if (!$response->getBody()->getContents() === 'Access is available') {
-            return response()->json(['error' => 'Incorrect.'], 401);
-        };
-
-        return response()->json(['success' => true, 'message' => 'Код отправлен.']);
-    }
+//    public function sendCode(Request $request)
+//    {
+//        $client = new Client();
+//        $response = $client->request('POST', 'https://iis.ngknn.ru/NGKNN/МамшеваЮС/MedicMadlab/api/SendCode', [
+//            'headers' => [
+//                'User-email' => $request->all()['email']
+//            ]
+//        ]);
+//
+//        if (!$response->getBody()->getContents() === 'Access is available') {
+//            return response()->json(['error' => 'Incorrect.'], 401);
+//        };
+//
+//        return response()->json(['success' => true, 'message' => 'Код отправлен.']);
+//    }
 }
