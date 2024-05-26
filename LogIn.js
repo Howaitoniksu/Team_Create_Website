1004b6b1583a7910c56210d9e0fb903d232a22f4
Howaitoniksu <129360255+Howaitoniksu@users.noreply.github.com>
2024-05-26 08:50:01 +0300

    make LogIn

--- LogIn.js
+++ LogIn.js
@@ -1,24 +1,20 @@
-async function register() {
-    const response = await fetch('http://localhost:8000/api/auth/register', {
+async function LogIn() {
+    const response = await fetch('http://localhost:8000/api/auth/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-            name: document.getElementById('name').value,
-            surname: document.getElementById('surname').value,
-            phone_number: document.getElementById('phone').value,
             email: document.getElementById('email').value,
             password: document.getElementById('password').value
         })
-    })
 
+    })
     const data = await response.json()
 
     if (!data.success) {
         return;
     }
-
     localStorage.setItem('token', data.token)
 
     window.location.href = 'mainPage.html'
