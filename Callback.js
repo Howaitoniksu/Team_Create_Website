6738c4d00e9ccffbb5fb35b99b2b16b74fa55c68
Howaitoniksu <vadimgorbachev.34@gmail.com>
2024-05-30 09:29:01 +0300

    +1 api script

--- Callback.js
+++ Callback.js
@@ -1,10 +1,10 @@
 async function CallbackInput() {  const response = await fetch('http://localhost:8000/api/Callback/StoreRequest', {
-    method: 'GET',
+    method: 'POST',
     headers: {
         'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-        token: localStorage.getItem('token'),
+        phone_number: document.getElementById(''),
     })
-}
+})
 }
