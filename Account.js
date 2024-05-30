035c21ed5a12c943cd717c4b78b86baa90358c2a
Howaitoniksu <vadimgorbachev.34@gmail.com>
2024-05-30 10:31:57 +0300

    bugfix

--- Account.js
+++ Account.js
@@ -1,17 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
+window.onload = () => 
+{
     AccountData()
-});
-
+}
 async function AccountData() {
     const response = await fetch('http://localhost:8000/api/user/getUserByToken', {
 
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
             token: localStorage.getItem('token'),
-        })
+        },
     })
     const data = await response.json()
     document.getElementById('name') = data.name,
