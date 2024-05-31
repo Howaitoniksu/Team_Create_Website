d114f5a285f04ee0843b829d21fc867e1dd8ccbd
ljubi <ljubivaya@outlook.com>
2024-05-31 22:13:53 +0300

    add postavka

--- AlterAuto/assets/json/teamAdding.js
+++ AlterAuto/assets/json/teamAdding.js
@@ -1,18 +1,26 @@
 window.onload = function() {
-    var popup = document.getElementById("myPopup");
-    var span = document.getElementsByClassName("close")[0];
+  var popup = document.getElementById("myPopup");
+  var span = document.getElementsByClassName("close")[0];
 
+  // Check if the popup has been shown before
+  var lastShown = localStorage.getItem("popupLastShown");
+  if (lastShown === null || new Date().getTime() - parseInt(lastShown) > 60000) {
+    // If not, show the popup
     window.setTimeout(function() {
-        popup.style.display = "block";
+      popup.style.display = "block";
     }, 3000); // Появление окна через 3 секунды
 
-    span.onclick = function() {
-        popup.style.display = "none";
-    }
+    // Set the timestamp of the last time the popup was shown
+    localStorage.setItem("popupLastShown", new Date().getTime());
+  }
 
-    window.onclick = function(event) {
-        if (event.target == popup) {
-            popup.style.display = "none";
-        }
+  span.onclick = function() {
+    popup.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == popup) {
+      popup.style.display = "none";
     }
+  }
 }
