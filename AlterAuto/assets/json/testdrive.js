842451bcae8664d9be48ec822a5708c4874d3ac5
ljubi <ljubivaya@outlook.com>
2024-05-31 09:02:14 +0300

    add buttons записаться, скачать файл

--- AlterAuto/assets/json/testdrive.js
+++ AlterAuto/assets/json/testdrive.js
@@ -1,7 +1,8 @@
 function formatPhoneNumber(input) {
-    let value = input.value.replace(/\D/g, '');
-    input.value = value; 
+    let value = input.value.replace(/\D/g, ''); // Убираем все нецифровые символы
+    input.value = value; // Устанавливаем очищенное значение в поле ввода
 }
+
 function checkDate(input) {
     const today = new Date();
     const selectedDate = new Date(input.value);
@@ -11,29 +12,28 @@ function checkDate(input) {
         input.value = today.toISOString().slice(0, 10);
     }
 }
-function sendForm() {
+     let csvData = []; // Массив для хранения данных
+
+function saveToCSV() {
     const name = nameInput.value;
     const phone = phoneInput.value;
     const date = dateInput.value;
-    const phoneRegex = /^(\+7|8)\d{10}$/; 
+    const phoneRegex = /^(\+7|8)\d{10}$/;
     if (!phoneRegex.test(phone)) {
         alert("Номер телефона должен быть в формате 89290390339 или +79290390339.");
         return; 
     }
-
     if (!name || !phone || !date) {
         alert("Все поля должны быть заполнены");
     } else {
-        alert("Форма отправлена!");
-        saveData(name, phone, date);
-        location.href = 'lifeAkk.html';
+    csvData.push([name, phone, date]);
+
+    alert("Записано");
     }
 }
-function saveData(name, phone, date) {
-    const data = [
-        [name, phone, date]
-    ];
-    const csvContent = data.map(row => row.join(",")).join("\n");
+
+function downloadCSV() {
+    const csvContent = csvData.map(row => row.join(",")).join("\n");
     const blob = new Blob([csvContent], { type: "text/csv" });
     const url = URL.createObjectURL(blob);
     const link = document.createElement("a");
